66ffd5306fb8b47fb7517d5d76e91f5163da5a2e
luaiabuelsamen <55265045+luaiabuelsamen@users.noreply.github.com>
2022-01-23 04:14:10 -0500

    frontend done

--- routes.py
+++ routes.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from markupsafe import string
+#from NLP_Twitter import getRating
 app = Flask(__name__)
 
+
+def getRating(s):
+    return 5
 # two decorators, same function
 
 
 @app.route('/')
-def base():
-    return render_template('home.html', the_title='risko homepage')
+def welcome():
+    return render_template('results.html')
+
+
+@app.route('/display', methods=['POST', 'GET'])
+def result():
+    coin = request.form.get("coin")
+    score = getRating(coin)
+    return render_template('results.html', score=score)
 
 
 if __name__ == '__main__':
